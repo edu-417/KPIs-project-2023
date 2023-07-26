d21a74ff1934a124c0f96bfa84859624690d6826
Eduardo Melendez <edgame127@gmail.com>
2023-07-26 10:45:51 -0500

    feat: added get copper price

--- scrap.py
+++ scrap.py
@@ -10,10 +10,8 @@ URL_BASE_ELECTRICITY = f"{URL_BASE_BCRP}/mensuales/resultados/PD37966AM/html"
 URL_BASE_INTERN_DEMAND = f"{URL_BASE_BCRP}/trimestrales/resultados/PN02529AQ/html"
 URL_BASE_UNEMPLOYEMENT_RATE = f"{URL_BASE_BCRP}/mensuales/resultados/PN38063GM/html"
 URL_INDEX_PRICE = "https://www.inei.gob.pe/media/MenuRecursivo/indices_tematicos/02_indice-precios_al_consumidor-nivel_nacional_2b_16.xlsx"
-now = datetime.datetime.now()
-# print(now)
-start = "2023-6"
-end = "2023-6"
+URL_COPPER_PRICE = "https://si3.bcentral.cl/Siete/ES/Siete/Cuadro/CAP_EI/MN_EI11/EI_PROD_BAS/637185066927145616"
+#?cbFechaInicio=2023&cbFechaTermino=2023&cbFrecuencia=MONTHLY&cbCalculo=NONE&cbFechaBase="
 
 
 def get_electricity(start, end):
@@ -62,12 +60,30 @@ def get_unemployment_rate(start, end):
     get_bcrp_data(start, end, URL_BASE_UNEMPLOYEMENT_RATE)
 
 
+def get_copper_price(start_year, end_year, frecuency="MONTHLY"):
+    url = f"{URL_COPPER_PRICE}?cbFechaInicio={start_year}&cbFechaTermino={end_year}&cbFrecuencia={frecuency}&cbCalculo=NONE&cbFechaBase="
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    cupper_row = soup.css.select('#tbodyGrid > tr:nth-of-type(4) > .ar')
+
+    for i, column in enumerate(cupper_row):
+        month = datetime.datetime(start_year, i + 1, 1).strftime('%B')
+        value = float( column.getText().strip().replace(',', '') ) / 10
+        print(f"{month}: {value}")
+
 def main():
+    #KPI 1
     get_electricity("2023-4", "2023-6")
+    #KPI 9
     get_pbi("202304")
+    #KPI 12
     get_intern_demand("2023-1", "2023-4")
-    get_price_index("Abril", 2023)
+    #KPI 13
     get_unemployment_rate("2023-1", "2023-6")
+    #KPI 18-19
+    get_price_index("Abril", 2023)
+    #KPI 20
+    get_copper_price(2023, 2023)
 
 if __name__ == "__main__":
     main()
